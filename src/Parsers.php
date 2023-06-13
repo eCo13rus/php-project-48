65162c57a60965326ee1042f386b7709bb82f0c2
Ilya Shirokov <asd.mat@mail.ru>
2023-06-13 11:56:43 +0300

    fix

--- src/Parsers.php
+++ src/Parsers.php
@@ -10,7 +10,6 @@ function convertFile(string $fileContent, string $extension): object
         case 'json':
             return json_decode($fileContent);
         case 'yaml':
-            return Yaml::parse($fileContent, Yaml::PARSE_OBJECT_FOR_MAP);
         case 'yml':
             return Yaml::parse($fileContent, Yaml::PARSE_OBJECT_FOR_MAP);
         default:
