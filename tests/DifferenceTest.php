8649b6694fc1d6ff4647ed188c413137a828ab31
Ilya Shirokov <asd.mat@mail.ru>
2023-06-16 17:24:25 +0300

    fix

--- tests/DifferenceTest.php
+++ tests/DifferenceTest.php
@@ -22,25 +22,25 @@ class DifferenceTest extends TestCase
          $firstPath = $this->getFixturePath($firstPath);
          $secondPath = $this->getFixturePath($secondPath);
          $expectedFile = $this->getFixturePath($expectedFile);
-    
+
          $this->assertStringEqualsFile($expectedFile, genDiff($firstPath, $secondPath, $style));
     }
-    
+
     public function argumentProvider(): array
     {
          return [
             ['file1_flat.json', 'file2_flat.json', 'result_formatter_stylish'],
             ['file1_flat.yaml', 'file2_flat.yml', 'result_formatter_stylish'],
-    
+
             ['file1_tree.json', 'file2_tree.json', 'result_stylish_formatter_tree'],
             ['file1_tree.json', 'file2_tree.json', 'result_stylish_formatter_tree', 'stylish'],
-    
+
             ['file1_tree.yaml', 'file2_tree.yaml', 'result_stylish_formatter_tree'],
             ['file1_tree.yaml', 'file2_tree.yaml', 'result_stylish_formatter_tree', 'stylish'],
-    
+
             ['file1_tree.json', 'file2_tree.json', 'result_plain_formatter_tree', 'plain'],
             ['file1_tree.yaml', 'file2_tree.yaml', 'result_plain_formatter_tree', 'plain'],
-    
+
             ['file1_tree.json', 'file2_tree.json', 'result_formatter_json', 'json'],
             ['file1_tree.yaml', 'file2_tree.yaml', 'result_formatter_json', 'json']
         ];
